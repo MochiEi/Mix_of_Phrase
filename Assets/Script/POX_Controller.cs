4122a697ea9c2988890d0230119feb33bf8a86f4
aaaa <137864418+exellents@users.noreply.github.com>
2023-12-12 12:08:30 +0900

    移動、ジャンプについて
    細かい調整までやりました

--- Assets/Script/POX_Controller.cs
+++ Assets/Script/POX_Controller.cs
@@ -1,45 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class POX_Controller : MonoBehaviour
 {
-    private float speed = 3.0f; //自機のスピード
+    public float speed;// = 3.0f; //自機のスピード
 
-    private Rigidbody2D rd;
+    private Rigidbody2D rb;
     private float jumpForce = 250f; //自機のジャンプ力
     private int jumpCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        rd = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Space) && this.jumpCount < 1)
         {
-            position.x -= speed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            position.x += speed * Time.deltaTime;
-        }
-
-        transform.position = position;
-
-        if (Input.GetKeyDown(KeyCode.Space)&&this.jumpCount<1)
-        {
-            this.rd.AddForce(transform.up * jumpForce);
+            this.rb.AddForce(transform.up * jumpForce);
             jumpCount++;
         }
     }
-    private void OnCollisionEnter2D(Collision2D other)
+
+    void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
             jumpCount = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            jumpCount = 0;
+        }
     }
 }
