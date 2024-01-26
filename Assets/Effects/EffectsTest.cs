ec23560a7fdbe9d126b75ac4a2885f098b9cf6ff
tagutinito <117792780+tagtagy@users.noreply.github.com>
2024-01-26 15:29:50 +0900

    a

---
+++ Assets/Effects/EffectsTest.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsTest : MonoBehaviour
+{
+
+    // プレハブ格納用
+    public GameObject BOXadvent;
+    public GameObject BOXbreak;
+    public GameObject FrameEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // 生成位置
+            Vector3 pos = this.transform.localPosition;
+            // プレハブを指定位置に生成
+            Instantiate(BOXadvent, pos, Quaternion.Euler(90f, 0f, 0f));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // 生成位置
+            Vector3 pos = this.transform.localPosition;
+            // プレハブを指定位置に生成
+            Instantiate(BOXbreak, pos, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // 生成位置
+            Vector3 pos = this.transform.localPosition;
+            // プレハブを指定位置に生成
+            Instantiate(FrameEffect, pos, Quaternion.identity);
+        }
+    }
+}
