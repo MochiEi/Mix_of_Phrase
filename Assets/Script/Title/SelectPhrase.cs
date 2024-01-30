22338bc9f1ef3b777001cb507d831391574f3f2b
MochiEi <home.ei0408@gmail.com>
2024-01-30 15:23:26 +0900

    Re.

---
+++ Assets/Script/Title/SelectPhrase.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPhrase : MonoBehaviour
+{
+
+    string text1;
+    string text2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Push1()
+    {
+
+    }
+    private void Push2()
+    {
+
+    }
+    private void Push3()
+    {
+
+    }
+    private void Push4()
+    {
+
+    }
+}
