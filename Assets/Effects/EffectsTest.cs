ada51f0c12781d5a99be29587518c91737b95096
tagutinito <117792780+tagtagy@users.noreply.github.com>
2024-02-02 15:19:31 +0900

    a

--- Assets/Effects/EffectsTest.cs
+++ Assets/Effects/EffectsTest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EffectsTest : MonoBehaviour
@@ -9,35 +10,67 @@ public class EffectsTest : MonoBehaviour
     public GameObject BOXadvent;
     public GameObject BOXbreak;
     public GameObject FrameEffect;
+
+    public float speed=3;
+    private Rigidbody2D rb;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             // 生成位置
             Vector3 pos = this.transform.localPosition;
             // プレハブを指定位置に生成
             Instantiate(BOXadvent, pos, Quaternion.Euler(-90f, 0f, 0f));
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             // 生成位置
             Vector3 pos = this.transform.localPosition;
             // プレハブを指定位置に生成
             Instantiate(BOXbreak, pos, Quaternion.identity);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             // 生成位置
             Vector3 pos = this.transform.localPosition;
             // プレハブを指定位置に生成
             Instantiate(FrameEffect, pos, Quaternion.identity);
         }
+
+
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        //右入力で左向きに動く
+        if (horizontalKey > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        //左入力で左向きに動く
+        else if (horizontalKey < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        //ボタンを話すと止まる
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    // 物理演算をしたい場合はFixedUpdateを使うのが一般的
+    void FixedUpdate()
+    {
+        
     }
 }
+
+
