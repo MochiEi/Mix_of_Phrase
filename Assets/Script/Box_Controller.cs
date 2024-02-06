5441f2f39ed2046105749b4f359727e41ba0034b
aaaa <137864418+exellents@users.noreply.github.com>
2024-02-06 11:38:41 +0900

    完成
    多分大丈夫

--- Assets/Script/Box_Controller.cs
+++ Assets/Script/Box_Controller.cs
@@ -10,16 +10,17 @@ public class Box_Controller : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField]private bool move = false;   //é©ï™Ç™ìÆÇ¢ÇƒÇ¢ÇÈÇ©Ç«Ç§Ç©
-    [SerializeField] private bool pushmove = false;   //é©ï™Ç™ìÆÇ¢ÇƒÇ¢ÇÈÇ©Ç«Ç§Ç©
     [SerializeField] private bool poxmove = false;
+    [SerializeField] private bool Lmove = false;
+    [SerializeField] private bool Rmove = false;
 
     [SerializeField] private bool wallstop = false;
-    [SerializeField] private bool boxside = false;
 
     private float Raylength = 0.05f;
 
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private LayerMask boxLayer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,13 +31,21 @@ public class Box_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (boxSide(boxside = true) && poxmove == false)
+        if (boxSideR() && poxmove == false)
         {
-            move = true;
+            Rmove = true;
         }
-        if (boxSide(boxside = true) == false || poxmove == true)
+        if(!boxSideR())
         {
-            move = false;
+            Rmove = false;
+        }
+        if (boxSideL() && poxmove == false)
+        {
+            Lmove = true;
+        }
+        if (!boxSideL())
+        {
+            Lmove = false;
         }
 
         if (poxSide())
@@ -48,14 +57,20 @@ public class Box_Controller : MonoBehaviour
             poxmove = false;
         }
 
-
-
-
-        if (move == true && wallstop == false || poxmove == true && wallstop == false)
+        if(wallStop()&&boxDown())
         {
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            wallstop= true;
         }
-        else
+
+
+        if (Rmove==true||Lmove==true||poxmove==true)
+        {
+            if(wallstop==false)
+            {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+        if(Rmove == false && Lmove == false && poxmove == false || wallstop==true)
         {
             rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
         }
@@ -72,19 +87,15 @@ public class Box_Controller : MonoBehaviour
         return BoxLdown.collider != null || BoxRdown.collider != null || BoxLup.collider != null || BoxRup.collider != null;
     }
 
-    private bool boxSide(bool x)    //BoxÇÃâ°Ç…boxÇ™Ç†ÇÈÇ∆true
+    private bool boxSideL()    //BoxÇÃâ°Ç…boxÇ™Ç†ÇÈÇ∆true
     {
         RaycastHit2D BoxLup = Physics2D.Raycast(new Vector2(transform.position.x - 0.401f, transform.position.y + 0.35f), Vector2.left, Raylength, boxLayer);
-        RaycastHit2D BoxRup = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y + 0.35f), Vector2.right, Raylength, boxLayer);
         RaycastHit2D BoxLdown = Physics2D.Raycast(new Vector2(transform.position.x - 0.401f, transform.position.y - 0.35f), Vector2.left, Raylength, boxLayer);
-        RaycastHit2D BoxRdown = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y - 0.35f), Vector2.right, Raylength, boxLayer);
 
-        if (x == true)
-        {
             if (BoxLup.collider != null)
             {
                 Box_Controller otherbox = BoxLup.collider.GetComponent<Box_Controller>();
-                if (otherbox.move == true || otherbox.poxmove == true)
+                if (otherbox.Lmove == true || otherbox.poxmove == true)
                 {
                     return true;
                 }
@@ -92,71 +103,45 @@ public class Box_Controller : MonoBehaviour
             if (BoxLdown.collider != null)
             {
                 Box_Controller otherbox = BoxLdown.collider.GetComponent<Box_Controller>();
-                if (otherbox.move == true || otherbox.poxmove == true)
-                {
-                    return true;
-                }
-            }
-            if (BoxRup.collider != null)
+            if (otherbox.Lmove == true || otherbox.poxmove == true)
             {
-                Box_Controller otherbox = BoxRup.collider.GetComponent<Box_Controller>();
-                if (otherbox.move == true || otherbox.poxmove == true)
-                {
-                    return true;
-                }
-            }
-            if (BoxRdown.collider != null)
-            {
-                Box_Controller otherbox = BoxRdown.collider.GetComponent<Box_Controller>();
-                if (otherbox.move == true || otherbox.poxmove == true)
-                {
-                    return true;
-                }
+                return true;
             }
         }
-
-        if (x == false)
-        {
-            if (BoxLup.collider != null)
-            {
-                Box_Controller otherbox = BoxLup.collider.GetComponent<Box_Controller>();
-                if (otherbox.wallstop == true)
-                {
-                    return true;
-                }
-            }
-            if (BoxLdown.collider != null)
-            {
-                Box_Controller otherbox = BoxLdown.collider.GetComponent<Box_Controller>();
-                if (otherbox.wallstop == true)
-                {
-                    return true;
-                }
-            }
-            if (BoxRup.collider != null)
-            {
-                Box_Controller otherbox = BoxRup.collider.GetComponent<Box_Controller>();
-                if (otherbox.wallstop == true)
-                {
-                    return true;
-                }
-            }
-            if (BoxRdown.collider != null)
-            {
-                Box_Controller otherbox = BoxRdown.collider.GetComponent<Box_Controller>();
-                if (otherbox.wallstop == true)
-                {
-                    return true;
-                }
-            }
-        }
-
         return false;
     }
+
+    private bool boxSideR()    //BoxÇÃâ°Ç…boxÇ™Ç†ÇÈÇ∆true
+    {
+        RaycastHit2D BoxRup = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y + 0.35f), Vector2.right, Raylength, boxLayer);
+        RaycastHit2D BoxRdown = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y - 0.35f), Vector2.right, Raylength, boxLayer);
+
+        if (BoxRup.collider != null)
+        {
+            Box_Controller otherbox = BoxRup.collider.GetComponent<Box_Controller>();
+            if (otherbox.Rmove == true || otherbox.poxmove == true)
+            {
+                Rmove = true;
+                return true;
+            }
+        }
+        if (BoxRdown.collider != null)
+        {
+            Box_Controller otherbox = BoxRdown.collider.GetComponent<Box_Controller>();
+            if (otherbox.Rmove == true || otherbox.poxmove == true)
+            {
+                Rmove = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
     private bool boxDown()    //BoxÇÃâ∫Ç…Ç»ÇÒÇ©Ç†ÇÈÇ∆true
     {
-        RaycastHit2D Box = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.401f), Vector2.down, -Raylength);
-        if(Box.collider != null)
+        RaycastHit2D BoxL = Physics2D.Raycast(new Vector2(transform.position.x - 0.35f, transform.position.y - 0.401f), Vector2.down, -Raylength);
+        RaycastHit2D BoxR = Physics2D.Raycast(new Vector2(transform.position.x + 0.35f, transform.position.y - 0.401f), Vector2.down, -Raylength);
+        if (BoxL.collider != null|| BoxR.collider != null)
         {
             return true;
         }
@@ -164,13 +149,53 @@ public class Box_Controller : MonoBehaviour
         
     }
 
-    private void OnCollisionStay2D(Collision2D collision)   //boxÇ™ï«Ç…êGÇÍÇƒâüÇπÇ»Ç¢èÛë‘
+    private bool wallStop() //ï«Ç…êGÇÍÇƒâüÇπÇ»Ç≠Ç»ÇÈÇ©Ç«Ç§Ç©        
     {
-        if (collision.gameObject.CompareTag("Wall") && boxDown())
+        RaycastHit2D BoxLup = Physics2D.Raycast(new Vector2(transform.position.x - 0.401f, transform.position.y + 0.35f), Vector2.left, Raylength);
+        RaycastHit2D BoxRup = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y + 0.35f), Vector2.right, Raylength);
+        RaycastHit2D BoxLdown = Physics2D.Raycast(new Vector2(transform.position.x - 0.401f, transform.position.y - 0.35f), Vector2.left, Raylength);
+        RaycastHit2D BoxRdown = Physics2D.Raycast(new Vector2(transform.position.x + 0.401f, transform.position.y - 0.35f), Vector2.right, Raylength);
+
+        if (BoxLup.collider != null&&BoxLdown.collider!=null) 
         {
-            wallstop = true;
+            if (BoxLup.collider.CompareTag("Wall")&& BoxLdown.collider.CompareTag("Wall"))
+            {
+                return true;
+            }
+            if (BoxLup.collider.CompareTag("Box") && BoxLdown.collider.CompareTag("Box"))
+            {
+                Box_Controller otherboxup = BoxLup.collider.GetComponent<Box_Controller>();
+                Box_Controller otherboxdown = BoxLdown.collider.GetComponent<Box_Controller>();
+                if (otherboxup.wallstop == true || otherboxdown.wallstop == true)
+                {
+                    return true;
+                }
+            }
         }
-        if (collision.gameObject.CompareTag("Box") && boxSide(boxside = false) && boxDown()==false)
+
+        if (BoxRup.collider != null && BoxRdown.collider != null)
+        {
+            if (BoxRup.collider.CompareTag("Wall") && BoxRdown.collider.CompareTag("Wall"))
+            {
+                return true;
+            }
+            if (BoxRup.collider.CompareTag("Box") && BoxRdown.collider.CompareTag("Box"))
+            {
+                Box_Controller otherboxup = BoxRup.collider.GetComponent<Box_Controller>();
+                Box_Controller otherboxdown = BoxRdown.collider.GetComponent<Box_Controller>();
+                if (otherboxup.wallstop == true || otherboxdown.wallstop == true)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") && wallStop())//boxÇâüÇπÇ»Ç¢èÛë‘
         {
             wallstop = true;
         }
@@ -189,6 +214,7 @@ public class Box_Controller : MonoBehaviour
         Gizmos.DrawRay(new Vector2(transform.position.x + 0.401f, transform.position.y), new Vector2(Raylength, 0));
 
         Gizmos.color = Color.green;
-        Gizmos.DrawRay(new Vector2(transform.position.x, transform.position.y - 0.401f), new Vector2(0,-Raylength));
+        Gizmos.DrawRay(new Vector2(transform.position.x - 0.35f, transform.position.y - 0.401f), new Vector2(0,-Raylength));
+        Gizmos.DrawRay(new Vector2(transform.position.x + 0.35f, transform.position.y - 0.401f), new Vector2(0, -Raylength));
     }
 }
