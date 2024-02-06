bdda7145fcf15dd87357fda74ede81f7e1f81148
MochiEi <home.ei0408@gmail.com>
2024-02-06 15:23:58 +0900

    titleClear

--- Assets/Script/Title/SelectPhrase.cs
+++ Assets/Script/Title/SelectPhrase.cs
@@ -1,39 +1,150 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class SelectPhrase : MonoBehaviour
 {
 
+    [SerializeField] private GameObject selectText; 
+    [SerializeField] private CanvasGroup canvas;
+
+    Text text;
     string text1;
     string text2;
+
+    public bool startFade;
+    public float FadeTime;
+    private int FadeCount;
+    private int textCount;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        text = selectText.GetComponent<Text>();
+        text1 = "click";
+        text2 = "phrase";
+        
+        if(!startFade) canvas.alpha = 0;
+        FadeCount = 0;
+        textCount = 1;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        text.text = text1 + " " + text2;
+
+        switch(FadeCount)
+        {
+            case 0:
+                fadeIn();
+                break;
+            case 1:
+                break;
+            case 2:
+                fadeOut(); 
+                break;
+        }
     }
-
-    private void Push1()
+    public void TrashPhrase()
     {
 
+        if (text.text == "game select")
+        {
+            FadeCount++;
+        }
+        else if (text.text == "game key")
+        {
+            print(text.text);
+        }
+        else if (text.text == "exit game")
+        {
+            ExitGame();
+        }
+        else
+        {
+            text1 = "click";
+            text2 = "phrase";
+            textCount = 1;
+        }
     }
-    private void Push2()
+    private void fadeIn()
     {
-
+        canvas.alpha += Time.deltaTime * FadeTime;
+        canvas.alpha = Math.Min(1, canvas.alpha);
+        if(canvas.alpha >= 1) FadeCount++;
     }
-    private void Push3()
+    private void fadeOut()
     {
-
+        canvas.alpha -= Time.deltaTime * FadeTime;
+        canvas.alpha = Math.Max(0, canvas.alpha);
+        if (canvas.alpha <= 0) SceneManager.LoadScene("Select");
     }
-    private void Push4()
+    private void ExitGame()
     {
-
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+    }
+    public void Push1()
+    {
+        if (textCount == 2)
+        {
+            text2 = "game";
+            textCount++;
+        }
+        if (textCount == 1)
+        {
+            text1 = "game";
+            text2 = null;
+            textCount++;
+        }
+    }
+    public void Push2()
+    {
+        if (textCount == 2)
+        {
+            text2 = "select";
+            textCount++;
+        }
+        if (textCount == 1)
+        {
+            text1 = "select";
+            text2 = null;
+            textCount++;
+        }
+    }
+    public void Push3()
+    {
+        if (textCount == 2)
+        {
+            text2 = "key";
+            textCount++;
+        }
+        if (textCount == 1)
+        {
+            text1 = "key";
+            text2 = null;
+            textCount++;
+        }
+    }
+    public void Push4()
+    {
+        if (textCount == 2)
+        {
+            text2 = "exit";
+            textCount++;
+        }
+        if (textCount == 1)
+        {
+            text1 = "exit";
+            text2 = null;
+            textCount++;
+        }
     }
 }
