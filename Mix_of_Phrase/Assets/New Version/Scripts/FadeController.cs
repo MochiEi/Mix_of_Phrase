be14ef5d47fb190feab6f7b358979adbc8c87c91
MochiEi <home.ei0408@gmail.com>
2024-10-11 13:47:38 +0900

    2024/10/11-2

--- Mix_of_Phrase/Assets/New Version/Scripts/FadeController.cs
+++ Mix_of_Phrase/Assets/New Version/Scripts/FadeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,35 @@ using UnityEngine.UI;
 
 public class FadeController : MonoBehaviour
 {
-    
+    private Image fadeImage;
 
     [SerializeField] float fadeTime;
+    private float amount;
 
     public bool isFade;
     public bool fadeIn;
     public bool fadeOut;
+    [SerializeField] bool testMode;
 
     void Start()
     {
-        
+        fadeImage = GetComponent<Image>();
+
+        if (!testMode)
+            fadeImage.fillAmount = 1;
     }
 
     void Update()
     {
-        
+        amount = fadeImage.fillAmount;
+        fadeChake();
+
+        fadeImage.raycastTarget = false;
+    }
+
+    private void fadeChake()
+    {
+        fadeIn = (fadeImage.fillAmount == 1);
+        fadeOut = (fadeImage.fillAmount == 0);
     }
 }
