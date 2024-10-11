1c97bc03da8536d737ff5bd806ddd5e6d0140ce5
MochiEi <home.ei0408@gmail.com>
2024-10-11 12:09:28 +0900

    2024/10/11-1

---
+++ Mix_of_Phrase/Assets/New Version/Scripts/FadeController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+    
+
+    [SerializeField] float fadeTime;
+
+    public bool isFade;
+    public bool fadeIn;
+    public bool fadeOut;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
