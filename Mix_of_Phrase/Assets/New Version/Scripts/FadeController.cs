491c03aa63b2baad18c6823df8748fe7eec0bb68
MochiEi <home.ei0408@gmail.com>
2024-10-15 14:28:25 +0900

    2024/10/15-1

--- Mix_of_Phrase/Assets/New Version/Scripts/FadeController.cs
+++ Mix_of_Phrase/Assets/New Version/Scripts/FadeController.cs
@@ -1,40 +1,85 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
 
 public class FadeController : MonoBehaviour
 {
-    private Image fadeImage;
+    private int skipFrame;
 
-    [SerializeField] float fadeTime;
+    private Image fadeImage;
     private float amount;
 
-    public bool isFade;
+    [SerializeField] float fadeSpeed;
+    public bool isActive;
+
+    /// フェードが引いてる状態
+    [HideInInspector]
     public bool fadeIn;
+    /// フェードが閉じてる状態
+    [HideInInspector]
     public bool fadeOut;
+
+    [Header("")]
     [SerializeField] bool testMode;
 
     void Start()
     {
         fadeImage = GetComponent<Image>();
+        amount = fadeImage.fillAmount;
 
-        if (!testMode)
-            fadeImage.fillAmount = 1;
+        amount = 1;
+        fadeSpeed = -Mathf.Abs(fadeSpeed);
+        isActive = true;
+
+        /// フェードイン・アウトをスキップ
+        if (testMode)
+        {
+            amount = 0;
+            isActive = false;
+        }
     }
 
     void Update()
     {
-        amount = fadeImage.fillAmount;
-        fadeChake();
+        /// カク付き防止のため5フレームスキップ
+        if(skipFrame < 5)
+        {
+            skipFrame++;
+            return;
+        }
 
-        fadeImage.raycastTarget = false;
+        if (isActive)
+            amount += fadeSpeed * Time.deltaTime;
+
+        amount = Mathf.Clamp(amount, 0, 1);
+
+        if (amount == 1 || amount == 0)
+            isActive = false;
+
+        /// フェード中はクリック操作してもイメージが邪魔して無効
+        if (fadeIn)
+            fadeImage.raycastTarget = false;
+        else
+            fadeImage.raycastTarget = true;
+
+        fadeImage.fillAmount = amount;
+        fadeChake();
+        fadeDirection();
     }
 
     private void fadeChake()
     {
-        fadeIn = (fadeImage.fillAmount == 1);
-        fadeOut = (fadeImage.fillAmount == 0);
+        fadeIn = (amount == 0);
+        fadeOut = (amount == 1);
+    }
+
+    private void fadeDirection()
+    {
+        if (fadeIn)
+            fadeSpeed = Mathf.Abs(fadeSpeed);
+        else if (fadeOut)
+            fadeSpeed = -Mathf.Abs(fadeSpeed);
     }
 }
