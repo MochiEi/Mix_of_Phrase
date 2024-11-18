7bcfed7e368614b6662d61432d2d2c5ca79636e5
MochiEi <home.ei0408@gmail.com>
2024-11-18 15:17:41 +0900

    2024/11/18-1

---
+++ Mix_of_Phrase/Assets/New Version/Scripts/Home/PhraseContoroller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhraseContoroller : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
