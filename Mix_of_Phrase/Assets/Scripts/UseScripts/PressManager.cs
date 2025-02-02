c2bad367d86805692c7010a5b5ab3d48ee5fe2a3
aran-ignition <137865219+aran-ignition@users.noreply.github.com>
2025-02-03 00:53:43 +0900

    a

--- Mix_of_Phrase/Assets/Scripts/UseScripts/PressManager.cs
+++ Mix_of_Phrase/Assets/Scripts/UseScripts/PressManager.cs
@@ -1,94 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class PressManager : MonoBehaviour
 {
+    
+    public List<string> TagNames;
     [SerializeField]
-    public List<GameObject> gameObjects = new List<GameObject>();
-    [SerializeField]
-    string[] tagList;
-    [SerializeField]
-    List<Collider2D> Addobj;
-    [SerializeField]
-    public List<bool> PushFlagList = new List<bool>();
+    public List<bool> PushFlagList;
 
-    private void Update()
+    public List<Collider2D> HitList;//箱たちが当たったオブジェクトの判定
+
+    void RemoveAtList<T>(List<T> list)
     {
-        // リストが10を超えないように制限
-        if (gameObjects.Count > 10)
+        if (list.Count > 10)
         {
-            gameObjects.RemoveAt(0); // インデックス0の要素を削除
-            PushFlagList.RemoveAt(0); // インデックス0のPushFlagも削除
+            list.RemoveAt(0);
         }
     }
 
-    // ヒットしたオブジェクトをリストに追加し、PushFlagを設定する
-    public void HitSet(GameObject HitObj)
+    public void Flagment(Collider2D coll)
     {
-        // すでに存在する場合は何もしない
-        if (!gameObjects.Contains(HitObj))
+        coll.OverlapCollider(new ContactFilter2D().NoFilter(), HitList);
+        TagNames.Clear();
+        for (int i = 0; i < HitList.Count; i++)
         {
-            gameObjects.Add(HitObj);
-
-            // Poxタグのオブジェクトが追加されたらPushFlagをtrueに
-            if (HitObj.CompareTag("Pox"))
-            {
-                PushFlagList.Add(true);
-            }
-            else
-            {
-                PushFlagList.Add(false); // それ以外のタグはfalse
-            }
+            TagNames.Add(HitList[i].tag);
         }
+        if (TagNames.Contains("Pox"))
+        {
+            PushFlagList.Add(true);
+        }
+        else
+        {
+            PushFlagList.Add(false);
+        }
+
+        RemoveAtList(HitList);
+        RemoveAtList(PushFlagList);
     }
 
-    // 離れたオブジェクトをリストから削除し、PushFlagを更新
-    public void OutSet(GameObject OutObj)
-    {
-        int index = gameObjects.IndexOf(OutObj);
-        if (index != -1)
-        {
-            // すでにPushFlagListに同じ状態のfalseがある場合は何もしない
-            if (PushFlagList[index] != false)
-            {
-                PushFlagList[index] = false;  // 離れた際にfalseに設定
-            }
-        }
-    }
-
-    // オブジェクトの重なりを検出してPushFlagを設定
-    public void Flagment(Collider2D col)
-    {
-        int count = col.OverlapCollider(new ContactFilter2D(), Addobj);
-
-        if (count > 0)
-        {
-            for (int i = 0; i < count; i++) // 修正: i <= count -> i < count
-            {
-                if (TargetTagResarch(Addobj[i], tagList))  // タグをチェック
-                {
-                    HitSet(Addobj[i].gameObject);
-                }
-                else
-                {
-                    OutSet(Addobj[i].gameObject);
-                }
-            }
-        }
-    }
-
-    // タグを調べるメソッド
-    private bool TargetTagResarch(Collider2D coll, string[] tagList)
-    {
-        foreach (string tag in tagList)
-        {
-            if (coll.CompareTag(tag))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
 }
